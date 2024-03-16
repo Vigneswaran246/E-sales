30b4884a9ef5d8dffd26d96bd73b6524d6bd87da
Vigneswaran <vengatkarthi81@gmail.com>
2024-03-16 19:11:11 +0530

    updates

---
+++ includes/sidebar.php
@@ -0,0 +1,12 @@
+<div class="container-fluid">
+    <a class="btn btn-dark" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample">
+        <i class="bi bi-list"></i>
+    </a>
+    <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+        <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
+            <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+
+    </div>
+</div>
--- index.php
+++ index.php
@@ -1,5 +1,10 @@
-<?php include_once './includes/header.php' ?>
+<?php
+
+include_once './includes/header.php';
+include_once './includes/sidebar.php';
+
+?>
 
 
 
-<?php include_once './includes/header.php' ?>
+<?php include_once './includes/header.php'; ?>
